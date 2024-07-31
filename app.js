f0343defe8332c59916b26cc546e09365711a5c1
AleenaArif30 <aleena.arif2024@gmail.com>
2024-08-01 00:08:00 +0500

    counter by using events of onclick

---
+++ app.js
@@ -0,0 +1,12 @@
+var counter = 0
+function increase(){
+    counter++
+    document.getElementById("counter").innerHTML = counter
+}
+
+function decrease(){
+    counter--
+    document.getElementById("counter").innerHTML = counter
+}
+
+alert("Hey! welcome")
